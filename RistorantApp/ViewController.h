e429f8133a59e5efc5a08024e818fd7a0d63bbf3
Miguel Serrato <jmserrato@narrowcastdigital.com>
2016-09-28 20:24:08 -0500

    Initial Commit

---
+++ RistorantApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RistorantApp
+//
+//  Created by NDM on 9/28/16.
+//  Copyright © 2016 NDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RistorantApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RistorantApp
+//
+//  Created by NDM on 9/28/16.
+//  Copyright © 2016 NDM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
